32d94b2b860aaa9cf5b8a333e8f646a71281dea7
Paul Le Guen de Kerneizon <paul-le-guen@outlook.fr>
2024-12-01 20:44:52 +0100

    day1: add second part solution
    Signed-off-by: Paul Le Guen de Kerneizon <paul-le-guen@outlook.fr>

--- day1/day1.py
+++ day1/day1.py
@@ -5,18 +5,32 @@ def first_part(first_col, second_col):
     total_distance = 0
 
     while len(first_col) != 0:
-        min_first_col = np.min(first_col)
-        min_first_col_index = np.argmin(first_col)
-        min_second_col = np.min(second_col)
-        min_second_col_index = np.argmin(second_col)
 
-        distance = abs(min_first_col - min_second_col)
+        min_first_col = search_min_value(first_col)
+        min_second_col = search_min_value(second_col)
+
+        distance = abs(min_first_col[1] - min_second_col[1])
         total_distance = total_distance + distance
-        first_col = np.delete(first_col,min_first_col_index)
-        second_col = np.delete(second_col,min_second_col_index)
+        first_col = np.delete(first_col,min_first_col[0])
+        second_col = np.delete(second_col,min_second_col[0])
     return total_distance
 
 
+def second_part(first_col, second_col):
+    similarity_score = 0
+
+    for first_col_val in first_col:
+        second_col_occurrences = search_coeffcient(second_col, first_col_val)
+        similarity_score = similarity_score + (first_col_val * second_col_occurrences)
+    return similarity_score
+
+def search_min_value(col):
+    min = np.min(col)
+    min_index = np.argmin(col)
+    return int(min_index),int(min)
+
+def search_coeffcient(col, val):
+    return np.count_nonzero(col == val)
 
 if __name__ == '__main__':
     data = np.genfromtxt("day1/data", dtype=int, encoding = None, delimiter="   ")
@@ -26,3 +40,5 @@ if __name__ == '__main__':
     answer1 = first_part(first_col, second_col)
     print(f"Solution for first part is {answer1}")
 
+    answer2 = second_part(first_col, second_col)
+    print(f"Solution for first part is {answer2}")
