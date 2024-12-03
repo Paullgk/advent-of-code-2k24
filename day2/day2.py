c35bceb3520701ab5c7650af35e2da04e7bbac08
Paul Le Guen de Kerneizon <paul-le-guen@outlook.fr>
2024-12-03 18:30:03 +0100

    day2: add second part solution
    Signed-off-by: Paul Le Guen de Kerneizon <paul-le-guen@outlook.fr>

--- day2/day2.py
+++ day2/day2.py
@@ -26,11 +26,23 @@ def check_safety(report):
 if __name__ == '__main__':
     f = open("day2/data", "r")
     data = f.read().splitlines()
-    safe = 0
+    answer1 = 0
+    answer2 = 0
     for report in data:
         report = list(map(int, report.split()))
+        safe = check_safety(report)
+        if safe is True:
+            answer1 += 1
+            answer2 += 1
+        if safe is False:
+            for level_index in range(0, len(report)):
+                modified_report = np.delete(report, level_index)
+                safe = check_safety(modified_report)
 
-        if check_safety(report):
-            safe += 1
+                if safe is True:
+                    print(f"Report {report} is safe by removing {report[level_index]}")
+                    answer2 += 1
+                    break
 
-    print(f"Solution for first part is {safe}")
+    print(f"Solution for first part is {answer1}")
+    print(f"Solution for first part is {answer2}")
